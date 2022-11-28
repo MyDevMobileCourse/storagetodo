7e35ee407b60057088113003af213d562ec7f41c
youssefchlendi <youssef.chlendi@gmail.com>
2022-11-28 11:01:15 +0100

    save to storage

--- app/src/main/java/com/example/internalstorage/MainActivity.kt
+++ app/src/main/java/com/example/internalstorage/MainActivity.kt
@@ -20,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var save:Button
     lateinit var clear:Button
     lateinit var load:Button
+    lateinit var sharedPref:SharedPref
+    var color = Color.WHITE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -40,23 +42,42 @@ class MainActivity : AppCompatActivity() {
         load = findViewById(R.id.load2)
         red.setOnClickListener {
             root.setBackgroundColor(Color.RED)
+            color = Color.RED
         }
         green.setOnClickListener {
             root.setBackgroundColor(Color.GREEN)
+            color = Color.GREEN
         }
         blue.setOnClickListener {
             root.setBackgroundColor(Color.BLUE)
+            color = Color.BLUE
         }
         black.setOnClickListener {
             root.setBackgroundColor(Color.BLACK)
+            color = Color.BLACK
         }
         white.setOnClickListener {
             root.setBackgroundColor(Color.WHITE)
+            color = Color.WHITE
         }
         count.setOnClickListener {
             var count = counter.text.toString().toInt()
             count++
             counter.text = count.toString()
+        }
+        save.setOnClickListener {
+            sharedPref = SharedPref(this)
+            sharedPref.save("counter",counter.text.toString())
+            sharedPref.save("color",color)
+        }
+        clear.setOnClickListener {
+            sharedPref = SharedPref(this)
+            sharedPref.clearSharedPreference()
+        }
+        load.setOnClickListener {
+            sharedPref = SharedPref(this)
+            counter.text = sharedPref.getValueInt("counter")
+            root.setBackgroundColor(sharedPref.getValueInt("color"))
         }
 
 
